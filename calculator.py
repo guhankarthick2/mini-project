15f059e7285da5b7a148ffe44562dc0a595c0e28
guhankarthick2 <guhankarthick2@gmail.com>
2026-06-14 18:58:43 -0500

    repeated calculations

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
+start = input("Do you want to start the calculator? (yes/no): ")
+while start.lower() == "yes":
+    num1 = (input("Enter first number:"))
+    operator1 = input("Enter operator (+, -, *, /):")
+    num2 = (input("Enter second number;"))
+    if operator1 == "+":
+        value = float(num1) + float(num2)
+    elif operator1 == "-":
+        value = float(num1) - float(num2)
+    elif operator1 == "*":
+        value = float(num1) * float(num2)
+    elif operator1 == "/":
+        value = float(num1) / float(num2)
+    else:
+        value = "Please enter a valid operator (No additional spaces)"
 
-num1 = (input("Enter first number:"))
-operator1 = input("Enter operator (+, -, *, /):")
-num2 = (input("Enter second number;"))
-if operator1 == "+":
-    value = float(num1) + float(num2)
-elif operator1 == "-":
-    value = float(num1) - float(num2)
-elif operator1 == "*":
-    value = float(num1) * float(num2)
-elif operator1 == "/":
-    value = float(num1) / float(num2)
-else:
-    value = "Please enter a valid operator (No additional spaces)"
+    print(f"The answer is {value}")
+    start =input("Do you want to continue? (yes/no): ")
 
-print(f"The answer is {value}")
