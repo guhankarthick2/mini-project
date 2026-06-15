e3f61a0dce02ceb5030231ec732be22ac37c2c3f
guhankarthick2 <guhankarthick2@gmail.com>
2026-06-15 17:50:12 -0500

    streak counter + bug fixes

--- multiplicationgame.py
+++ multiplicationgame.py
@@ -1,34 +1,46 @@
 import random
+
 questionsCount = input("How many questions? ")
 questionsAnswered = 0
 correctAnswered = 0
+correctStreak = 0
+compliment = ["You're on fire!", "Great Job!", "Excellent!","Too easy?","Genius level!"]
 difficulty = input("Chooose a difficulty (easy, medium, hard, or extreme)  ")
 while questionsAnswered < int(questionsCount):
-    if difficulty == "easy":
+    if difficulty.lower() == "easy":
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
-    elif difficulty == "medium":
+    elif difficulty.lower() == "medium":
         num1 = random.randint(5, 20)
         num2 = random.randint(5, 20)
-    elif difficulty == "hard":
+    elif difficulty.lower() == "hard":
         num1 = random.randint(100,1000 )
         num2 = random.randint(100,1000)
-    elif difficulty == "extreme":
+    elif difficulty.lower() == "extreme":
         num1 = random.randint(1000, 10000)
         num2 = random.randint(1000, 10000)
     else:
         print("Invalid difficulty, easy has automatically been chosen")
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
-    answer = input(f"{num1} * {num2}  ")
+    answer = input(f"Question {questionsAnswered + 1}: {num1} * {num2}  ")
     if float(answer) == (num1 * num2):
         print("Correct!")
         correctAnswered += 1
+        correctStreak += 1
     else:
         print("Incorrect!")
+        correctStreak = 0
+
+    if correctStreak >= 3:
+        print(f"{correctStreak} in a row, {compliment[random.randint(0,4)]}")
 
     questionsAnswered += 1
     print(f'{correctAnswered} out of {questionsAnswered} correct!')
 
-if correctAnswered == questionsAnswered:
+percentage = correctAnswered / questionsAnswered * 100
+print(f"{percentage}% correct")
+if percentage == 100:
     print("Great Job, everything is correct!")
+
+
